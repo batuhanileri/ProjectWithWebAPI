54ff83d5d48b2bb04d130671b38bfd7936e83d7d
batu_ <batu_@DESKTOP-AOMM71G>
2022-04-21 11:11:34 +0300

    DataAccess Created

---
+++ DataAccess/Concrete/Contexts/WebAPIContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Contexts
+{
+    public class WebAPIContext:DbContext
+    {
+        public WebAPIContext(DbContextOptions<WebAPIContext> options) : base(options)
+        {
+
+        }
+        public WebAPIContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string conStr = "Data Source=DESKTOP-AOMM71G; Initial Catalog = WebAPIContextDb; Integrated Security =true";
+            optionsBuilder.UseSqlServer(conStr);
+        }
+    }
+}
--- Entities/Dtos/UserDtos/UserUpdateDto.cs
+++ Entities/Dtos/UserDtos/UserUpdateDto.cs
@@ -15,7 +15,6 @@ namespace Entities.Dtos.UserDtos
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
         public string Address { get; set; }
         public bool Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
